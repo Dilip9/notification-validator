84037ead1ffe976aa7b588d88d90ef9b109a901a
Dilip9 <dilip.verma940805@gmail.com>
2025-12-27 23:47:44 +0530

    Initial commit

---
+++ src/main/java/gouri/notification/notification_validator/NotificationValidatorApplication.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gouri/notification/notification_validator/NotificationValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
